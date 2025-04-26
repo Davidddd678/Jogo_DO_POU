ebccc89f505cd96b1d2a5c455695eff7cc5ea5b3
David Marques <david.marques@students.assembly.pt>
2025-04-26 10:50:38 +0100

    Initial commit

---
+++ classes.py
@@ -0,0 +1,213 @@
+import pygame
+import sys
+import time
+
+class Criatura:
+
+    def __init__(self,nome):
+        self.nome = nome
+        self.felicidade1 = 0
+        self.fome1 = 0
+        self.sujidade1 = 0
+        self.odio = 50
+        self.lista = [self.felicidade1, self.fome1, self.sujidade1, self.ódio]
+
+
+    def alimentar1(self):
+        self.alimentar1 -= 5
+
+    def carinho1(self):
+        self.carinho1 += 20
+
+    def sujidade1(self):
+        self.sujidade1 += 50
+
+    def odio(self):
+        self.ódio += 20
+
+class Pou(Criatura):
+    def __init__(self, nome):
+        super().__init__(nome)
+class Pou:
+
+    def __init__(self, niveldefome, felicidade, nome, sujidade, rebeldia, carinho,amor):
+        self.nome = nome
+        self.niveldefome = int(niveldefome)
+        self.felicidade = int(felicidade)
+        self.sujidade = int(sujidade)
+        self.rebeldia = int(rebeldia)
+        self.carinho = int(carinho)
+        self.amor = int(amor)
+
+    def perda(self):
+        if self.niveldefome >= 100:
+            print("O teu pou está a morrer de fome, LITERALMENTE!!!")
+            pygame.time.delay(1000)
+            print("Ele morreu de fome")
+            pygame.quit()
+            sys.exit()
+        if self.niveldefome <= 0:
+            print("O teu pou ESTÁ CHEIO!!!")
+            pygame.time.delay(1000)
+            print("Ele morreu de comer")
+            pygame.quit()
+            sys.exit()
+        if self.felicidade >= 100:
+            print("O teu pou está demasiado feliz, ELE DEITOU VÔMITO ARCO-ÍRIS")
+            pygame.time.delay(1000)
+            print("Ele morreu de felicidade")
+            pygame.quit()
+            sys.exit()
+        if self.felicidade <= 0:
+            print("Ele precisa de amigos ")
+            pygame.time.delay(1000)
+            print("Ele morreu de tédio")
+            pygame.quit()
+            sys.exit()
+        if self.sujidade >= 100:
+            print("Ele está demasiado limpo para uma criatura como ele!!!")
+            pygame.time.delay(1000)
+            print("Ele morreu de lindez")
+            pygame.quit()
+            sys.exit()
+        if self.sujidade <= 0:
+            print("Ele precisa de tomar banho rapidamente !!!")
+            pygame.time.delay(1000)
+            print("Ele morreu de sujidade")
+            pygame.quit()
+            sys.exit()
+        if self.rebeldia >= 100:
+            print("Ele está muito rebelde!")
+            pygame.time.delay(1000)
+            print("Ele morreu num tiroteio de forma suja")
+            pygame.quit()
+            sys.exit()
+        if self.rebeldia <= 0:
+            print("Ele precisa de rebeldia!!!")
+            pygame.time.delay(1000)
+            print("Ele morreu de falta de rebeldia")
+            pygame.quit()
+            sys.exit()
+        if self.carinho >= 100:
+            print("Ele têm demasiado carinho acumulado!!!!")
+            pygame.time.delay(1000)
+            print("Ele morreu de carinho")
+            pygame.quit()
+            sys.exit()
+        if self.carinho <= 0:
+            print("Ele precisa de algum carinho!!!")
+            pygame.time.delay(1000)
+            print("Ele morreu por falta de carinho")
+            pygame.quit()
+            sys.exit()
+        if self.amor >= 100:
+            print("Ele têm demasiado amores !!!")
+            pygame.time.delay(1000)
+            print("Ele morreu 'pelo amor'")
+            pygame.quit()
+            sys.exit()
+        if self.amor <= 0:
+            print("Ele precisa de algum amor !!!")
+            pygame.time.delay(1000)
+            print("Ele morreu por falta de amor")
+            pygame.quit()
+            sys.exit()
+
+    def limites(self):
+        if self.niveldefome < 0:
+            self.niveldefome = 0
+        elif self.niveldefome > 100:
+            self.niveldefome = 100
+        if self.felicidade < 0:
+            self.felicidade = 0
+        elif self.felicidade > 100:
+            self.felicidade = 100
+        if self.sujidade < 0:
+            self.sujidade = 0
+        elif self.sujidade > 100:
+            self.sujidade= 100
+        if self.rebeldia < 0:
+            self.rebeldia = 0
+        elif self.rebeldia > 100:
+            self.rebeldia = 100
+        if self.carinho < 0:
+            self.carinho = 0
+        elif self.carinho > 100:
+            self.carinho = 100
+        if self.amor < 0:
+            self.amor = 0
+        elif self.amor > 100:
+            self.amor = 100
+
+
+
+
+    def alimentar(self):
+        if self.niveldefome > 0:
+            self.niveldefome -= 10
+            print("Alimentaste o Teu pou!")
+
+
+
+    def banho(self):
+        if self.sujidade > 0:
+            self.sujidade -= 50
+            print("O teu pou tomou banho!")
+
+    def brincar(self):
+        if self.felicidade > 0:
+            self.felicidade += 20
+            print("O teu pou brincou")
+
+
+    def miudodagangue(self):
+        if self.rebeldia > 0:
+            self.rebeldia += 25
+            print("O teu pou foi rebelde")
+
+
+
+
+    def coracao(self):
+        if self.carinho > 0:
+            self.carinho += 20
+            print ("O pou recebeu carinho")
+
+    def brainrot(self):
+        print(
+            "Only in ohio "" IM HIM "" Are you skibidi "" IMAGINE IF NINJA GOT A LOWWWWWWWW TAPERRRRRRRR FADEEEEEEEEEE "" Eye of rah "" I buyed a proprety in Egypt and what they do for you is they give you the proprety "" If he doesn´t hawk tuah i don´t talk tuah "" MANGO MANGO MANGO"" Artist you can sing VS Artists who can´t sing"" Do you have any relationships whit your father.  What´s that,  What´s a father?" " No my hotspot" " We just got 8 free pizzas" " I knew that door had a lock on it and everbody made me think I was losing my mind!")
+
+    def amorverdadeiro(self):
+        if self.amor > 0:
+            self.amor += 20
+            print(" O teu pou recebeu amor")
+
+    def jogar(self):
+        ()
+
+    def stats(self):
+        print(f"Felicidade: {self.felicidade}")
+        print(f"nivel de fome: {self.niveldefome}")
+        print(f"sujidade: {self.sujidade}")
+        print(f"rebeldia: {self.rebeldia}")
+        print(f"carinho: {self.carinho}")
+        print(f"amor: {self.amor}")
+
+
+
+
+
+print(f""" Instruções para o jogo
+
+    [B]rincar - aumenta a felicidade
+    [A]limentar - diminui o niveldefome
+    [Ba]nho - diminui a sujidade
+    [G]angue - faz o teu_pou virar rebelde
+    [Br]ainrot- o teu_pou dirá uma frase de brainrot
+    [C]arinho - faz carinho ao teu_pou
+    [Am]or - dá o amor verdadeiro ao teu pou
+    [S]tatus - mostra os status do teu_pou
+    """)
+
+
+
---
+++ main.py
@@ -0,0 +1,222 @@
+import pygame
+import sys
+
+
+import classes
+meu_pou = classes.Pou(50,50, "nome",51,50,50, 50)
+
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Jogo do POU")
+
+# Cores
+BRANCO = (255, 255, 255)
+AZUL = (100, 100, 255)
+AZUL_MOUSE = (150, 150, 255)
+
+cena = "MENU"
+
+image = pygame.image.load('pou.png')
+image_rect = image.get_rect(center=(400, 200))
+
+image1 = pygame.image.load('mortopou.png')
+
+image2 = pygame.image.load('um-quarto-lindamente-decorado-apresentando-uma-mistura-de-design-de-interiores-moderno-e-aconchegante.png')
+tamanho_fundo = (800, 600)
+image2 = pygame.transform.scale(image2, tamanho_fundo)
+
+casado = pygame.image.load("POU casado.png")
+
+logo = pygame.image.load('Menu.imagem.png')
+tamanho_logo = (800, 600)
+logo = pygame.transform.scale(logo, tamanho_logo)
+
+
+Carinho = pygame.Rect(300, 400, 150, 50)
+Alimentar = pygame.Rect(300, 480, 150, 50)
+Brainrot = pygame.Rect(100, 400, 150, 50)
+Brincar = pygame.Rect(100, 480, 150, 50)
+Banho = pygame.Rect(300, 550, 150, 50)
+Rebelde = pygame.Rect(550, 400, 150, 50)
+Amor = pygame.Rect(550, 480, 150, 50)
+Jogar = pygame.Rect(300, 400, 200, 50)
+def Carinho_clicked():
+    meu_pou.coracao()
+    meu_pou.limites()
+    meu_pou.perda()
+
+
+
+def Alimentar_clicked():
+    meu_pou.alimentar()
+    meu_pou.limites()
+    meu_pou.perda()
+
+def Brainrot_clicked():
+    meu_pou.brainrot()
+    print("Parabéns!! Destruis-te a mente do teu pou!!!!!")
+
+
+def Brincar_clicked():
+    meu_pou.brincar()
+    meu_pou.limites()
+    meu_pou.perda()
+
+def Banho_clicked():
+    meu_pou.banho()
+    meu_pou.limites()
+    meu_pou.perda()
+
+
+def Rebelde_clicked():
+    meu_pou.miudodagangue()
+    meu_pou.limites()
+    meu_pou.perda()
+
+
+
+def Amor_clicked():
+    meu_pou.amorverdadeiro()
+    meu_pou.limites()
+    meu_pou.perda()
+
+def Jogar_clicked():
+    print(" Então vamos começar o jogo!!!")
+
+
+
+
+
+running = True
+while running:
+    screen.fill(BRANCO)
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+
+                if cena == "JOGO":
+                    if Carinho.collidepoint(event.pos):
+                        Carinho_clicked()
+                    if Alimentar.collidepoint(event.pos):
+                        Alimentar_clicked()
+                    if Brainrot.collidepoint(event.pos):
+                        Brainrot_clicked()
+                    if Brincar.collidepoint(event.pos):
+                        Brincar_clicked()
+                    if Rebelde.collidepoint(event.pos):
+                        Rebelde_clicked()
+                    if Amor.collidepoint(event.pos):
+                        Amor_clicked()
+                    if Banho.collidepoint(event.pos):
+                        Banho_clicked()
+
+                if cena == "MENU":
+                    if Jogar.collidepoint(event.pos):
+                        Jogar_clicked()
+                        cena = "JOGO"
+
+    mouse_pos = pygame.mouse.get_pos()
+
+
+
+
+    font = pygame.font.SysFont('Arial', 30)
+    text1 = font.render("Carinho", True, (255, 255, 255))
+    text2 = font.render("Alimentar", True, (255, 255, 255))
+    text3 = font.render("Brainrot", True, (255, 255, 255))
+    text4 = font.render("Brincar", True, (255, 255, 255))
+    text5 = font.render("Rebelde", True, (255, 255, 255))
+    text6 = font.render("Amor", True, (255, 255, 255))
+    text7 = font.render("Banho", True, (255, 255, 255))
+    text = font.render("Jogar", True, (255, 255, 255))
+
+
+
+
+
+    texto_pou = font.render(f" Brincar {meu_pou.felicidade}     Níveldefome:  {meu_pou.niveldefome}  Sujidade:  {meu_pou.sujidade}  " , False, (0,0,0,))
+    texto_pou1 = font.render(f"Carinho:     {meu_pou.carinho} Rebelde:  {meu_pou.rebeldia} Amor:  {meu_pou.amor}", False, (0,0,0))
+
+    texto_pou2 = font.render("DAVID MARQUES, O CRIADOR", True, (0,0,0))
+
+    if cena == "MENU":
+        screen.blit(texto_pou2, (100, 300))
+        screen.blit(logo, (0, 0))
+
+        if Jogar.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, AZUL_MOUSE, Jogar)
+        else:
+            pygame.draw.rect(screen, AZUL, Jogar)
+
+        screen.blit(text, (Jogar.centerx - text.get_width() // 2, Jogar.centery - text.get_height() // 2))
+
+
+
+
+
+
+    elif cena == "JOGO":
+        screen.blit(image2, (0, 0))
+        if meu_pou.amor > 50:
+            screen.blit(casado, image_rect)
+        else:
+            screen.blit(image, image_rect)
+        if Carinho.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, AZUL_MOUSE, Carinho)
+        else:
+            pygame.draw.rect(screen, AZUL, Carinho)
+
+        if Alimentar.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, AZUL_MOUSE, Alimentar)
+        else:
+            pygame.draw.rect(screen, AZUL, Alimentar)
+
+        if Brainrot.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, AZUL_MOUSE, Brainrot)
+        else:
+            pygame.draw.rect(screen, AZUL, Brainrot)
+
+        if Brincar.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, AZUL_MOUSE, Brincar)
+        else:
+            pygame.draw.rect(screen, AZUL, Brincar)
+
+        if Rebelde.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, AZUL_MOUSE, Rebelde)
+        else:
+            pygame.draw.rect(screen, AZUL, Rebelde)
+
+        if Amor.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, AZUL_MOUSE, Amor)
+        else:
+            pygame.draw.rect(screen, AZUL, Amor)
+
+        if Banho.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, AZUL_MOUSE, Banho)
+        else:
+            pygame.draw.rect(screen, AZUL, Banho)
+
+        screen.blit(text1, (Carinho.centerx - text1.get_width() // 2, Carinho.centery - text1.get_height() // 2))
+        screen.blit(text2, (Alimentar.centerx - text2.get_width() // 2, Alimentar.centery - text2.get_height() // 2))
+        screen.blit(text3, (Brainrot.centerx - text3.get_width() // 2, Brainrot.centery - text3.get_height() // 2))
+        screen.blit(text4, (Brincar.centerx - text4.get_width() // 2, Brincar.centery - text4.get_height() // 2))
+        screen.blit(text5, (Rebelde.centerx - text5.get_width() // 2, Rebelde.centery - text5.get_height() // 2))
+        screen.blit(text6, (Amor.centerx - text6.get_width() // 2, Amor.centery - text6.get_height() // 2))
+        screen.blit(text7, (Banho.centerx - text6.get_width() // 2, Banho.centery - text7.get_height() // 2))
+        screen.blit(texto_pou, (0, 0))
+        screen.blit(texto_pou1, (0, 50))
+
+
+    pygame.display.flip()
+
+
+pygame.quit()
+sys.exit()
